6001e1acc1d442fb18e64ba0ddccaeb5a392fc32
sapper6fd <justin@gallow.ca>
2021-05-20 12:59:52 -0600

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,15 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+
+            // Add a cors policey with any method, any header, from the requesting URL
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +64,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
